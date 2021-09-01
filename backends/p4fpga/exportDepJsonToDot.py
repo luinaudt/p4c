1730a3f021bc95b6cd1fc6963bacce260e7e69c9
Thomas Luinaud <thomas.luinaud@gmail.com>
2021-09-01 13:41:01 -0400

    generates info about number of edges

--- backends/p4fpga/exportDepJsonToDot.py
+++ backends/p4fpga/exportDepJsonToDot.py
@@ -17,9 +17,12 @@ limitations under the License.
 import networkx
 import json
 import getopt
+import argparse
 import sys
 import os
 from pathlib import Path
+
+global options
 
 def convertFolder(inputFolder, outputFolder, deparserName):
     if not os.path.exists(outputFolder):
@@ -35,10 +38,11 @@ def convertFolder(inputFolder, outputFolder, deparserName):
                           deparserName)
         outFile = Path(os.path.join(outputFolder, i))
         outFile = str(outFile.parent.joinpath(outFile.stem + ".dot"))
-        convertFile(str(os.path.join(inputFolder,i)), outFile ,deparserName)
+        processFile(str(os.path.join(inputFolder,i)), outFile ,deparserName)
         
-
-def convertFile(inputFile, outputFile, deparserName):
+def getGraph(inputFile, deparserName):
+    """ return networkx Graph
+    """
     if Path(inputFile).suffix != ".json":
         print("{} is not a json suffix".format(inputFile))
         print("exit convertion")
@@ -47,48 +51,43 @@ def convertFile(inputFile, outputFile, deparserName):
     with open(inputFile,'r') as f:
         deparser = json.load(f)[deparserName]
     graph = networkx.readwrite.json_graph.node_link_graph(deparser["graph"], directed=True)
+    return graph
+
+def processFile(inputFile, outputFile, deparserName):
+    graph = getGraph(inputFile,deparserName)
+    if options.countEdges:
+        print(f"{inputFile} : edges = {networkx.number_of_edges(graph)}")
+    if options.toDot:
+        genDot(graph, outputFile, deparserName)
+
+def genDot(graph, outputFile, deparserName):
     networkx.drawing.nx_agraph.write_dot(graph, outputFile)
 
 def convert(fileName = "a.out", outfileName= "a.dot", deparserName = "deparser"):
     if os.path.isdir(fileName):
         convertFolder(fileName, outfileName, deparserName)
     else:
-        convertFile(fileName, outfileName, deparserName)
+        processFile(fileName, outfileName, deparserName)
     
 
-def main(prog, argv):
-    inputFile = "a.out"
-    outputFile = "a.json"
-    deparserName = "deparser"
-    if len(argv) <=0 :
-        print("argument are required")
-        print(f"{prog} [-i inputFile] [-o outputFile] [--deparserName name]")
-        sys.exit(2)
-    try:
-        opts, _= getopt.getopt(argv, "ho:i:",
-                                        ["help", "outputFile", "inputFile",
-                                        "deparserName"])
-    except getopt.GetoptError:
-        print(f"{prog} [-i inputFile] [-o outputFile] [--deparserName name]")
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt in ("-h", "--help"):
-            print(f"{prog} [-i inputFile] [-o outputFile] [--deparserName name]")
-            sys.exit(0)
-        elif opt in ("-o", "--outputFile"):
-            outputFile = arg
-        elif opt == "--deparserName":
-            deparserName = arg
-        elif opt in ("-i", "--inputFile"):
-            inputFile = arg
-    if os.path.exists(inputFile):
-        convert(inputFile, outputFile, deparserName)
+def main(argv):
+    global options
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--input", "-i", help="input file or folder of Json source files", required=True)
+    parser.add_argument("--output", "-o", help="output file or folder results", required=True)
+    parser.add_argument("--deparserName", help="name of the deparser", default="deparser", required=False)
+    parser.add_argument("--countEdges", help="activate edges count", action="store_true", required=False)
+    parser.add_argument("--notToDot", dest="toDot", help="deactivate dot export", action="store_false", required=False)
+    args, otherArgs = parser.parse_known_args()
+    options=args
+    if os.path.exists(args.input):
+        convert(args.input, args.output, args.deparserName)
     else:
-        print("{} does not exists".format(inputFile))
+        print("{} does not exists".format(args.input))
         sys.exit(1)
     
 
 if __name__ == "__main__":
     # execute only if run as a script
-    main(sys.argv[0], sys.argv[1:])
+    main(sys.argv)
     exit()
--- backends/p4fpga/test/paper_src/includes/headers.p4
+++ backends/p4fpga/test/paper_src/includes/headers.p4
@@ -33,6 +33,21 @@ header ipv4_t {
     ip4Addr_t srcAddr;
     ip4Addr_t dstAddr;
 }
+//conformity with paper
+header ipv4_h {
+    bit<4>    version;
+    bit<4>    ihl;
+    bit<8>    diffserv;
+    bit<16>   totalLen;
+    bit<16>   identification;
+    bit<3>    flags;
+    bit<13>   fragOffset;
+    bit<8>    ttl;
+    bit<8>    protocol;
+    bit<16>   hdrChecksum;
+    ip4Addr_t srcAddr;
+    ip4Addr_t dstAddr;
+}
 
 
 header tcp_h {
